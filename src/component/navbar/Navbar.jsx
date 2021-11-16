dd41e74f7981900a2c10f548687aed1b111a6891
haluskua <haluskua.one@gmail.com>
2021-11-17 12:20:05 +1300

    added svg logo and assets folder with images

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import "./navbar.css";
 
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
+//BEM --> Block ELement Modifier
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="modern_navbar">
+      <div className="modern__navbar-links">
+        <div className="modern__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
