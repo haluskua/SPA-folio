70d533aa18fe3203160f569bf13cf3f673c9dfd1
haluskua <haluskua.one@gmail.com>
2021-11-26 07:19:48 +1300

    added Blog component starting

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Article } from "../../component";
 import "./blog.css";
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="ompa__blog section__padding">
+      <div className="ompa__blog-heading">
+        <h1 className="gradient__text">A lot is happening</h1>
+      </div>
+      <div className="ompa__blog-container">
+        <div className="ompa__blog-container_groupA">
+          <Article />
+        </div>
+        <div className="ompa__blog-container_groupB">
+          <Article />
+          <Article />
+          <Article />
+          <Article />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
