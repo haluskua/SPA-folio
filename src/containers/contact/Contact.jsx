f6f4689b330e1c2d4eff0531a156a6a069eb947e
haluskua <haluskua.one@gmail.com>
2021-11-25 16:25:59 +1300

    added contact section with styling

--- src/containers/contact/Contact.jsx
+++ src/containers/contact/Contact.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import contactImage from "../../assets/x.png";
+import "../contact/contact.css";
 
 const Contact = () => {
   return (
     <div className="ompa__contact section__padding" id="Contact">
       <div className="ompa__contact-img">
         <img src={contactImage} alt="" alt="contact" />
+      </div>
+      <div className="ompa__contact-content">
+        <h4>Lorem, ipsum </h4>
+        <h1 className="gradient__text">Contact Me</h1>
+        <p>Lorem ipsum dolor sit amet consectetur elit. Voluptatem.</p>
+        <h4>Request early to work on project together</h4>
       </div>
     </div>
   );
