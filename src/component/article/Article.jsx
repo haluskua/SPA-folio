324c20639ccb9470acf674fba9042fcef8e80604
haluskua <haluskua.one@gmail.com>
2021-11-26 21:28:55 +1300

    addeed articles for the blog page and styling for blog and article with media queries

--- src/component/article/Article.jsx
+++ src/component/article/Article.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import "./article.css";
 
-const Article = ({ imgUrl }) => {
+const Article = ({ imgUrl, date, title }) => {
   return (
     <div className="ompa__blog-container_article">
       <div className="ompa__blog-container_article-image">
         <img src={imgUrl} alt="blog_image" />
+      </div>
+      <div className="ompa__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
       </div>
     </div>
   );
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -6,18 +6,33 @@ const Blog = () => {
   return (
     <div className="ompa__blog section__padding">
       <div className="ompa__blog-heading">
-        <h1 className="gradient__text">A lot is happening</h1>
+        <h1 className="gradient__title">A lot is happening</h1>
       </div>
       <div className="ompa__blog-container">
         <div className="ompa__blog-container_groupA">
-          <Article />
+          <Article imgUrl={blog01} />
         </div>
         <div className="ompa__blog-container_groupB">
-          <Article imgUrl={blog01} />
-          <Article imgUrl={blog02} />
-          <Article imgUrl={blog03} />
-          <Article imgUrl={blog04} />
-          <Article imgUrl={blog05} />
+          <Article
+            imgUrl={blog02}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us explore how it is?"
+          />
+          <Article
+            imgUrl={blog03}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us explore how it is?"
+          />
+          <Article
+            imgUrl={blog04}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us explore how it is?"
+          />
+          <Article
+            imgUrl={blog05}
+            date="Sep 26, 2021"
+            title="GPT-3 and Open  AI is the future. Let us explore how it is?"
+          />
         </div>
       </div>
     </div>
